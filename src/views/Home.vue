18909c97906caae0049e5aa2a37e15a8ee1422c7
ymym <ymym@kakao.com>
2021-03-14 20:51:08 +0900

    home에 버튼 추가

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
-    <h1>Welcome~~</h1>
+    <h1>Welcome to {{title}}!</h1>
+    <input type="text" v-model="input1" />
+    <button type="button" @click="getData()">Get</button>
+    <button type="button" @click="setData()">Set</button>
   </div>
 </template>
 
+
 <script>
 export default {
-name: "Home"
+  data(){
+    return{
+      title: "뷰 스터디 ",
+      title2: "ym",
+      input1: "input data"
+    };
+  },
+  methods:{
+    getData() {
+      alert(this.input1);
+    },
+    setData() {
+      this.input1="setData";
+    }
+  }
 }
 </script>
 
