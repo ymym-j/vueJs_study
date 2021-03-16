a41e418935912a7702236a6e55b9ad83e0cb3180
ymym <ymym@kakao.com>
2021-03-16 14:45:30 +0900

    table, select study

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,18 @@
     <input type="text" v-model="input1" />
     <button type="button" @click="getData()">Get</button>
     <button type="button" @click="setData()">Set</button>
+
+    <select class="form-control" v-model="region" @change="changeRegion">
+      <option :key="i" :value="d.v" v-for="(d, i) in options">{{d.t}}</option>
+    </select>
+    <!-- v if : false시 랜더링 x
+         v show : false시 랜더링 후 숨김 -->
+    <table class="table table-bordered" v-if="tableShow">
+      <tr :key="i" v-for="(d, i) in options">
+        <td>{{d.v}}</td>
+        <td>{{d.t}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -11,10 +23,17 @@
 <script>
 export default {
   data(){
-    return{
+    return {
       title: "뷰 스터디 ",
       title2: "ym",
-      input1: "input data"
+      input1: "input data",
+      options:[
+        {v:"S",t:"Seoul"},
+        {v:"J",t:"Jeju"},
+        {v:"B",t:"Busan"},
+      ],
+      region: "J",
+      tableShow: true,
     };
   },
   methods:{
@@ -23,9 +42,20 @@ export default {
     },
     setData() {
       this.input1="setData";
+    },
+    changeRegion(){
+      alert(this.region);
+    },
+  },
+  watch:{
+    input1(){
+      console.log(this.input1);
+    },
+    title(){
+
     }
   }
-}
+};
 </script>
 
 <style scoped>
